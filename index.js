383501a4fdd6d96446281c8305709188c91b666d
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-18 13:04:27 +0600

    mongo db code

--- index.js
+++ index.js
@@ -10,6 +10,38 @@ app.use(cors())
 app.use(express.json())
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_KEY}@cluster0.p45io4t.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const BabyCarCruiseCollation = client.db("babyCarCollation").collection("babyCar")
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/',(req, res )=>{
     res.send("Baby Toy Car Shop Is Open")
 })
