bf309c3c540cbe5cdd1b5374f87ec8c7709fd686
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-19 23:32:17 +0600

    add dynamic catagory

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000 ;
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
+//
+app.use(cors());
+app.use(express.json());
 
-// 
-app.use(cors())
-app.use(express.json())
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_KEY}@cluster0.p45io4t.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,19 +17,60 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const BabyCarCruiseCollation = client.db("babyCarCollation").collection("babyCar")
-
-
 async function run() {
+  const BabyCarCruiseCollation = client
+    .db("babyCarCollation")
+    .collection("babyCar");
+
+  app.get("/allCategories", async (req, res) => {
+    const query = {};
+    const result = await BabyCarCruiseCollation.find(query).toArray();
+    res.send(result);
+  });
+
+  app.get("/category/:categoryName", async (req, res) => {
+    const category = req.params.categoryName;
+    const query = { categoryName: category };
+    const result = await BabyCarCruiseCollation.find(query).toArray();
+    res.send(result);
+  });
+
+  app.get("/user/:email", async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const result = await BabyCarCruiseCollation.find(query).toArray();
+    res.send(result);
+  });
+
+  app.post("/category", async (req, res) => {
+    const items = req.body;
+    const result = await BabyCarCruiseCollation.insertOne(items);
+    res.send(result);
+  });
+
+  app.put("category/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+  });
+
+  app.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await BabyCarCruiseCollation.deleteOne(query);
+    res.send(result);
+  });
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -40,11 +78,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req, res )=>{
-    res.send("Baby Toy Car Shop Is Open")
-})
-app.listen(port,()=>{
-    console.log(`Toy Car Server Is Running Port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Baby Toy Car Shop Is Open");
+});
+app.listen(port, () => {
+  console.log(`Toy Car Server Is Running Port ${port}`);
+});
